2deeb8e2be8eefe9be5cab99bc9b1fb6d9a4d950
irinaIvanova97 <44892329+irinaIvanova97@users.noreply.github.com>
2019-02-01 16:47:18 +0200

    Initial commit

---
+++ persistence/src/main/java/ers/students/App.java
@@ -0,0 +1,13 @@
+package ers.students;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
