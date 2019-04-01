2d5071615183f64c4cf6904e91ce4d94e36c05d0
MartinStoynov <45785752+MartinStoynov@users.noreply.github.com>
2019-04-01 14:00:10 +0300

    Fixed status code phrase

--- endpoint/src/main/java/ers/students/factory/ResponseFactory.java
+++ endpoint/src/main/java/ers/students/factory/ResponseFactory.java
@@ -20,11 +20,6 @@ public class ResponseFactory {
      */
     private static final ResponseFactory instance = new ResponseFactory();
 
-    /**
-     * The constructor is final so that the class cannot be instantiated.
-     */
-    private ResponseFactory() {
-    }
 
     /**
      * Returns the only object available.
@@ -35,30 +30,25 @@ public class ResponseFactory {
         return instance;
     }
 
-    
     /**
      * Returns a HTTP response based on the given result.
      * @param result
      * @return 
      */
-    public static Response makeResponse(LoadResult result) {
+    public static Response make(LoadResult result) {
 
         if (result.getErrors().keySet().contains(ErrorCode.INTERNAL_ERROR)) {
-            
-            String message = "There was an internal server error.";
 
             return Response
                     .status(Response.Status.INTERNAL_SERVER_ERROR)
-                    .entity(message)
+                    .entity(result.getErrors().get(ErrorCode.INTERNAL_ERROR))
                     .build();
 
         } else if (result.getErrors().keySet().contains(ErrorCode.NO_SUCH_ELEMENT)) {
             
-            String message = "There is no such element.";
-            
             return Response
                     .status(Response.Status.NOT_FOUND)
-                    .entity(message)
+                    .entity(result.getErrors().get(ErrorCode.NO_SUCH_ELEMENT))
                     .build();
 
         } else if (result.getErrors().keySet().contains(ErrorCode.VALIDATION)) {
@@ -67,7 +57,7 @@ public class ResponseFactory {
             
             return Response
                     .status(Response.Status.BAD_REQUEST)
-                    .entity(message)
+                    .entity(result.getErrors().get(ErrorCode.VALIDATION))
                     .build();
 
         } else {
