36e23a4a62af51be6e43b4dee9aaa0583e9d4346
irinaIvanova97 <44892329+irinaIvanova97@users.noreply.github.com>
2019-04-08 10:29:48 +0300

    Added AbstractProviderTest.java; In AbstractCrudProvider.java added method loadAll(); Fixed method in SearchingCrudProvider to be abstract; In LoadResult.java and LoadResults.java replaced methods setErrors with addAllErrors; Removed unnecessary methods in PortfolioProviderTest.java and PositionProviderTest.java; Added static Portfolio object in SuccessfulPortfolioDao.java

--- business-logic/src/test/java/ers/students/crud/test/AbstractProviderTest.java
+++ business-logic/src/test/java/ers/students/crud/test/AbstractProviderTest.java
@@ -110,5 +110,6 @@ public abstract class AbstractProviderTest<E extends Validatable> extends TestCa
 
     private boolean CheckIfEntityIsPosition() {
         return entity.getClass().equals(Position.class);
-    }    
+    }
+    
 }
