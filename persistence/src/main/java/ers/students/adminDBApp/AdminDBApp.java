c79c5a9a70d00aae08fc963b0c98f4d55c0d3fee
ayhan21 <47269197+ayhan21@users.noreply.github.com>
2019-04-10 13:22:33 +0300

    Implementations in AdminDBApp class

--- persistence/src/main/java/ers/students/adminDBApp/AdminDBApp.java
+++ persistence/src/main/java/ers/students/adminDBApp/AdminDBApp.java
@@ -5,10 +5,39 @@
  */
 package ers.students.adminDBApp;
 
+import ers.students.persistentStore.JdbcPersistentStore;
+import ers.students.persistentStore.PersistentStore;
+import java.sql.SQLException;
+
 /**
+ * This class lets the administrator create a database by writing the URL of the
+ * DB, username and password
  *
  * @author Ayhan
  */
 public class AdminDBApp {
-    
+
+    static PersistentStore persistentStore;
+
+    private static void initJDBCPersistentStore(String dbURL, String userName, String password) {
+        persistentStore = new JdbcPersistentStore(dbURL, userName, password);
+    }
+
+    public static void main(String[] args) {
+        String url = "jdbc:hsql:file:dbtest";
+        String user = "SA";
+        String pass = "admin";
+        initJDBCPersistentStore(url, user, pass);
+        try {
+            persistentStore.startTransaction();
+            persistentStore.createDB();
+            persistentStore.commitTransaction();
+        } catch (SQLException ex) {
+            persistentStore.rollbackTransaction();
+        } finally {
+            if (persistentStore.getConnection() != null) {
+                persistentStore.close();
+            }
+        }
+    }
 }
