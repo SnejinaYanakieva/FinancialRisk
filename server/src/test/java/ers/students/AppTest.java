702e5d4b6ce9176753a0465cf518f26bd234e9ae
MartinStoynov <45785752+MartinStoynov@users.noreply.github.com>
2019-05-07 14:21:34 +0300

    added server module

---
+++ server/src/test/java/ers/students/AppTest.java
@@ -0,0 +1,14 @@
+package ers.students;
+
+import junit.framework.TestCase;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest extends TestCase
+{
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
