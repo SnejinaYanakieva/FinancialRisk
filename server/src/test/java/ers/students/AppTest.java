93153ef48abf33aa7569942f8db5cf54e38562bd
MartinStoynov <45785752+MartinStoynov@users.noreply.github.com>
2019-05-07 14:28:30 +0300

    fixed server test prototype

--- server/src/test/java/ers/students/AppTest.java
+++ server/src/test/java/ers/students/AppTest.java
@@ -1,14 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package ers.students;
 
 import junit.framework.TestCase;
 
 /**
- * Unit test for simple App.
+ *
+ * @author martinstoynov
  */
-public class AppTest extends TestCase
-{
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+public class AppTest extends TestCase {
+    
+    public AppTest(String testName) {
+        super(testName);
     }
+    
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+    
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public void testMain() {
+        assertEquals(4,4);
+    }
+    
 }
