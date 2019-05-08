b4b84a1b0ccfa8c4038f6157d30835b20eb07698
ayhan21 <47269197+ayhan21@users.noreply.github.com>
2019-05-08 13:55:37 +0300

    Created portfolioTransactionLoader method

--- persistence/src/main/java/ers/students/loader/PortfolioTransactionLoader.java
+++ persistence/src/main/java/ers/students/loader/PortfolioTransactionLoader.java
@@ -5,10 +5,19 @@
  */
 package ers.students.loader;
 
+import ers.students.portfolio.Portfolio;
+import ers.students.portfolio.Position;
+import ers.students.portfolio.Transaction;
+import java.util.List;
+import java.util.Map;
+
 /**
  *
  * @author Ayhan
  */
 public class PortfolioTransactionLoader {
-    
+
+    public Map<Portfolio, List<Transaction>> portfolioTransactionLoader(Position position) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 }
