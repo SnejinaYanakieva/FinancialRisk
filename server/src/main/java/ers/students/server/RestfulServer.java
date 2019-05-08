fa69f77907b8e3bb5967506abaeadfcc8312ebd4
DarkNexsus <47350806+DarkNexsus@users.noreply.github.com>
2019-05-08 14:04:55 +0300

    By broken hope that its going to work + Server:all my work

--- server/src/main/java/ers/students/server/RestfulServer.java
+++ server/src/main/java/ers/students/server/RestfulServer.java
@@ -10,7 +10,20 @@ package ers.students.server;
  * @author martinstoynov
  */
 public class RestfulServer {
-    public static void main(String args[]) throws Exception {
-        // code snippets shown above
-    }
+  
+        
+        //Otdeleni ,object  server ,i da addna serviceite, votdelni metodi da sa vs  
+
+JAXRSServerFactoryBean sf = new JAXRSServerFactoryBean();
+sf.setResourceClasses(ResponseFactory.class,FxQuoteRestServiceImpl.class);
+sf.setResourceProvider(ResponseFactory.class, new SingletonResourceProvider(ResponseFactory.getInstance()));
+sf.setAddress("http://localhost:9000/");
+BindingFactoryManager manager = sf.getBus().getExtension(BindingFactoryManager.class);
+JAXRSBindingFactory factory = new JAXRSBindingFactory();
+factory.setBus(sf.getBus());
+manager.registerBindingFactory(JAXRSBindingFactory.JAXRS_BINDING_ID, factory);
+sf.create();
 }
+
+}
+
